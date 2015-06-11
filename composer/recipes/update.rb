8134c95edb8702d3ada31fa2094e2125976b4454
Raffaele <izziaraffaele@gmail.com>
2015-06-12 00:00:40 +0200

    composer::update add ssh key

--- composer/recipes/update.rb
+++ composer/recipes/update.rb
@@ -3,6 +3,37 @@
 # http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
 #
 node[:deploy].each do |application, deploy|
+  directory "#{deploy[:home]}/.ssh" do
+    owner deploy[:user]
+    group deploy[:group]
+    mode "0700"
+    action :create
+    recursive true
+  end
+
+  file "#{deploy[:home]}/.ssh/config" do
+    owner deploy[:user]
+    group deploy[:group]
+    action :touch
+    mode '0600'
+  end
+
+  execute "echo 'StrictHostKeyChecking no' > #{deploy[:home]}/.ssh/config" do
+    not_if "grep '^StrictHostKeyChecking no$' #{deploy[:home]}/.ssh/config"
+  end
+
+  template "#{deploy[:home]}/.ssh/id_dsa" do
+    action :create
+    mode '0600'
+    owner deploy[:user]
+    group deploy[:group]
+    cookbook "scm_helper"
+    source 'ssh_key.erb'
+    variables :ssh_key => deploy[:ssh_key]
+    not_if do
+      deploy[:ssh_key].blank?
+    end
+  end
   script "install_composer" do
     interpreter "bash"
     user "root"
