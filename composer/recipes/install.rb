de23b1cbd96363fc4e8ebd71d1c572796caa9009
Raffaele <izziaraffaele@gmail.com>
2015-06-11 23:39:25 +0200

    Dio!

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -7,14 +7,36 @@ node[:deploy].each do |application, deploy|
     interpreter "bash"
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
-    if deploy[:scm]
-      ensure_scm_package_installed(deploy[:scm][:scm_type])
-      prepare_git_checkouts(
-        :user => deploy[:user],
-        :group => deploy[:group],
-        :home => deploy[:home],
-        :ssh_key => deploy[:scm][:ssh_key]
-      ) if deploy[:scm][:scm_type].to_s == 'git'
+    directory "#{deploy[:home]}/.ssh" do
+      owner deploy[:user]
+      group deploy[:group]
+      mode "0700"
+      action :create
+      recursive true
+    end
+
+    file "#{deploy[:home]}/.ssh/config" do
+      owner deploy[:user]
+      group deploy[:group]
+      action :touch
+      mode '0600'
+    end
+
+    execute "echo 'StrictHostKeyChecking no' > #{deploy[:home]}/.ssh/config" do
+      not_if "grep '^StrictHostKeyChecking no$' #{deploy[:home]}/.ssh/config"
+    end
+
+    template "#{deploy[:home]}/.ssh/id_dsa" do
+      action :create
+      mode '0600'
+      owner deploy[:user]
+      group deploy[:group]
+      cookbook "scm_helper"
+      source 'ssh_key.erb'
+      variables :ssh_key => deploy[:ssh_key]
+      not_if do
+        deploy[:ssh_key].blank?
+      end
     end
     code <<-EOH
     curl -s https://getcomposer.org/installer | php
