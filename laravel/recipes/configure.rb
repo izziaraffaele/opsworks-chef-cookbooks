3d601ab3e0dea093a4db89af89d444c289c8eb12
Raffaele <izziaraffaele@gmail.com>
2015-06-27 08:45:55 +0200

    Fix deploy folder

--- laravel/recipes/configure.rb
+++ laravel/recipes/configure.rb
@@ -5,12 +5,12 @@ node[:deploy].each do |application, deploy|
   include_recipe 'composer::install'
 
   # correct permissions to allow apache to write
-  execute "chown #{path}/app/storage" do
-      cwd "#{path}/app/storage"
+  execute "chown #{deploy[:deploy_to]}/app/storage" do
+      cwd "#{deploy[:deploy_to]}/app/storage"
       command "chown -R deploy.www-data ."
   end
-  execute "chmod #{path}/app/storage" do
-      cwd "#{path}/app/storage"
+  execute "chmod #{deploy[:deploy_to]}/app/storage" do
+      cwd "#{deploy[:deploy_to]}/app/storage"
       command "chmod -R u+rwX,g+rwX ."
   end
 end
