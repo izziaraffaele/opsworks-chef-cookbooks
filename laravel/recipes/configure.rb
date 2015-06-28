8e73c829bf6f6299fb4e6a79266797ec7393cebe
Raffaele <izziaraffaele@gmail.com>
2015-06-28 09:20:30 +0200

    permissions bootstrap cache

--- laravel/recipes/configure.rb
+++ laravel/recipes/configure.rb
@@ -13,4 +13,14 @@ node[:deploy].each do |application, deploy|
       cwd "#{deploy[:deploy_to]}/storage"
       command "chmod -R u+rwX,g+rwX ."
   end
+
+  # correct permissions to allow apache to write
+  execute "chown #{deploy[:deploy_to]}/bootstrap/cache" do
+      cwd "#{deploy[:deploy_to]}/bootstrap/cache"
+      command "chown -R  ."
+  end
+  execute "chmod #{deploy[:deploy_to]}/bootstrap/cache" do
+      cwd "#{deploy[:deploy_to]}/bootstrap/cache"
+      command "chmod -R u+rwX,g+rwX ."
+  end
 end
