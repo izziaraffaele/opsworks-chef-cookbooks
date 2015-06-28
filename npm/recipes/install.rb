75d32831268efb04c7a0c069a1a437f6a872765d
Raffaele <izziaraffaele@gmail.com>
2015-06-28 16:31:43 +0200

    add npm install

---
+++ npm/recipes/install.rb
@@ -0,0 +1,16 @@
+#
+# Taken from:
+# http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
+#
+
+node[:deploy].each do |application, deploy|
+  script "install_composer" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    npm install 
+    EOH
+    only_if { ::File.exists?("#{deploy[:deploy_to]}/current/package.json") }
+  end
+end 
