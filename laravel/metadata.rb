f0cf2ced4510dfd8234217442c4e87227a86bb47
Raffaele <izziaraffaele@gmail.com>
2015-06-28 17:41:36 +0200

    add deps to laravel

--- laravel/metadata.rb
+++ laravel/metadata.rb
@@ -6,6 +6,8 @@ description       "Set of recipes to help setup and deploy Laravel applications.
 version           "1.0.0"
 
 depends "composer"
+depends "npm"
 
 recipe "composer::install", "Installs composer packages."
 recipe "composer::update", "Update composer packages."
+recipe "npm::install", "Install npm packages."
