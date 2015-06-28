6e686505c01490b15a48f451fc6db3a5c4368740
Raffaele <izziaraffaele@gmail.com>
2015-06-28 09:29:11 +0200

    fix paths

--- laravel/recipes/configure.rb
+++ laravel/recipes/configure.rb
@@ -5,22 +5,22 @@ node[:deploy].each do |application, deploy|
   include_recipe 'composer::install'
 
   # correct permissions to allow apache to write
-  execute "chown #{deploy[:deploy_to]}/storage" do
-      cwd "#{deploy[:deploy_to]}/storage"
+  execute "chown #{deploy[:deploy_to]}/current/storage" do
+      cwd "#{deploy[:deploy_to]}/current/storage"
       command "chown -R  ."
   end
-  execute "chmod #{deploy[:deploy_to]}/storage" do
-      cwd "#{deploy[:deploy_to]}/storage"
+  execute "chmod #{deploy[:deploy_to]}/current/storage" do
+      cwd "#{deploy[:deploy_to]}/current/storage"
       command "chmod -R u+rwX,g+rwX ."
   end
 
   # correct permissions to allow apache to write
-  execute "chown #{deploy[:deploy_to]}/bootstrap/cache" do
-      cwd "#{deploy[:deploy_to]}/bootstrap/cache"
+  execute "chown #{deploy[:deploy_to]}/current/bootstrap/cache" do
+      cwd "#{deploy[:deploy_to]}/current/bootstrap/cache"
       command "chown -R  ."
   end
-  execute "chmod #{deploy[:deploy_to]}/bootstrap/cache" do
-      cwd "#{deploy[:deploy_to]}/bootstrap/cache"
+  execute "chmod #{deploy[:deploy_to]}/current/bootstrap/cache" do
+      cwd "#{deploy[:deploy_to]}/current/bootstrap/cache"
       command "chmod -R u+rwX,g+rwX ."
   end
 end
